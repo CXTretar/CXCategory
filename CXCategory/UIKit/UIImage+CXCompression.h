de68892f98f9b63929d375101c8ff23e246cb5bf
CXTretar <misscxuan@163.com>
2019-01-10 23:52:46 +0800

    切换git账号测试

--- CXCategory/UIKit/UIImage+CXCompression.h
+++ CXCategory/UIKit/UIImage+CXCompression.h
@@ -12,7 +12,14 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface UIImage (CXCompression)
 
-+ (UIImage *)cx_compressImage:(UIImage *)image toByte:(NSUInteger)maxLength
+/**
+ 压缩图片精确至指定Data大小, 只需循环3次, 并且保持图片不失真
+
+ @param image     原图
+ @param maxLength 目标图片Data大小
+ @return          压缩后的新图
+ */
++ (UIImage *)cx_compressImage:(UIImage *)image toByte:(NSUInteger)maxLength;
 
 @end
 
